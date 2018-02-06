97a47fef08a6b6d78fad7a176e309ae771e1709d
chintam anand <chintamanand56@gmail.com>
2018-02-06 20:20:31 +0530

    Check the tree is binary or not

---
+++ Check tree is bst or not/src/checkbst/Checkbst.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkbst;
+import java.util.*;
+ class Node{
+        Node left,right;
+        int data;
+     public Node(int data){
+      left=right=null;
+      this.data=data;
+     }
+    }
+public class Checkbst {
+    static Node insert(Node root,int data){
+       Node cur;
+    if(root==null){
+     return new Node(data);
+    }
+    else{
+     if(data <= root.data){
+         cur=insert(root.left,data);
+         root.left=cur;
+        }else{
+         cur=insert(root.right,data);
+         root.right=cur;
+        }
+        return root;
+    }
+   }
+   static boolean isbst(Node root){
+    Node curr=root;
+    if(curr==null){
+      return true;
+     }
+     
+      if(curr.left==null && curr.right==null){
+       return true;}
+      
+      if(curr.left!=null && curr.right!=null)
+       return (isbst(curr.left) && isbst(curr.right));
+      
+     return false;
+     
+       //return 0;
+   }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of node in the tree: ");
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            System.out.println("The given tree is binary tree or not: "+isbst(root));
+    }
+    
+}
