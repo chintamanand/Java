cd292b1128aae1fbcac7b243eb6d4eeb31969d78
chintam anand <chintamanand56@gmail.com>
2018-03-23 22:56:27 +0530

    Update Crazstring.java

--- CrazyString/src/crazstring/Crazstring.java
+++ CrazyString/src/crazstring/Crazstring.java
@@ -8,13 +8,7 @@ package crazstring;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/**
- *
- * @author mahe
- */
 public class Crazstring {
-/*package whatever //do not write package name here */
 
 	public static void main (String[] args) {
 	    Scanner sc = new Scanner(System.in);
