339f87d92027c6e9d3b3c947910a0bb03dcd1a10
chintam anand <chintamanand56@gmail.com>
2018-07-15 19:22:18 +0530

    Update ArraylistOperations.java

--- ArraylistOperation/src/arraylistoperations/ArraylistOperations.java
+++ ArraylistOperation/src/arraylistoperations/ArraylistOperations.java
@@ -3,11 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package arraylistoperations;
 import java.util.*;
 public class ArraylistOperations {
-
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scan=new Scanner(System.in);
