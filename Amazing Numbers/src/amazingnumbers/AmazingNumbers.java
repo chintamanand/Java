cb26a302708455d363a35a66e362ec5a264a3045
chintam anand <chintamanand56@gmail.com>
2018-09-01 22:27:53 +0530

    Update AmazingNumbers.java

--- Amazing Numbers/src/amazingnumbers/AmazingNumbers.java
+++ Amazing Numbers/src/amazingnumbers/AmazingNumbers.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 package amazingnumbers;
 import java.util.*;
 public class AmazingNumbers {
